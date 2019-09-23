fbf2f35e8db7a5459c3cf8ce11fbf0823787db52
web-dev <dias.07.04@gmail.com>
2019-09-23 17:11:21 +0600

    grazhdanka

--- controllers/new_form.js
+++ controllers/new_form.js
@@ -18,8 +18,9 @@ angular.module("app").controller("newFormCtrl", function ($scope, $http, $timeou
             })
     };*/
 
-    //Спиок категории дела
 
+    //Список категорий дел
+    $scope.item = {};
     $scope.getCaseCategoriesList = function () {
         $http({
             url: 'https://api.zandylyq.kz/v1/civil/case-categories',
@@ -40,11 +41,61 @@ angular.module("app").controller("newFormCtrl", function ($scope, $http, $timeou
             console.log(reason)
         });
     };
-
     $scope.getCaseCategoriesList();
 
 
-    $scope.sendResultCivil = function (item) {
+    //Список Судов
+    $scope.item = {};
+    $scope.getCourtsList = function () {
+        $http({
+            url: 'https://api.zandylyq.kz/v1/civil/courts',
+            method: 'GET',
+            cache: false,
+            contentType: false,
+            async: true,
+            processData: false,
+            headers: {
+                'Access-Control-Allow-Origin': true,
+                'Content-Type': 'application/json; charset=utf-8',
+                "X-Requested-With": "XMLHttpRequest"
+            }
+        }).then(function (value) {
+            $scope.courts = value.data;
+            console.log($scope.courts)
+        }, function (reason) {
+            console.log(reason)
+        });
+    };
+    $scope.getCourtsList();
+
+
+    //Список Результата рассмотрения
+
+    $scope.item = {};
+    $scope.getReviewResults = function () {
+        $http({
+            url: 'https://api.zandylyq.kz/v1/civil/review-results',
+            method: 'GET',
+            cache: false,
+            contentType: false,
+            async: true,
+            processData: false,
+            headers: {
+                'Access-Control-Allow-Origin': true,
+                'Content-Type': 'application/json; charset=utf-8',
+                "X-Requested-With": "XMLHttpRequest"
+            }
+        }).then(function (value) {
+            $scope.reviewResults = value.data;
+            console.log($scope.reviewResults)
+        }, function (reason) {
+            console.log(reason)
+        });
+    };
+    $scope.getReviewResults();
+
+
+    $scope.sendResultCivil = function (item, dateModel) {
         var modalInstance = $uibModal.open({
             animation: $scope.animationsEnabled,
             templateUrl: 'myModalCivil.html',
@@ -53,6 +104,10 @@ angular.module("app").controller("newFormCtrl", function ($scope, $http, $timeou
             resolve: {
                 value: function () {
                     return item
+                },
+
+                date: function () {
+                    return dateModel
                 },
                 serverURL: function () {
                     return $scope.serverURL;
@@ -74,15 +129,49 @@ angular.module("app").controller("newFormCtrl", function ($scope, $http, $timeou
     }
 });
 
-var modalCivil = function ($scope, $uibModalInstance, $http, value) {
+var modalCivil = function ($scope, $uibModalInstance, $http, value, date) {
+
+
+    if (date) {
+        value.date_from = date.date_from;
+        value.date_to = date.date_to;
+
+
+        var mm = ((value.date_from.getMonth() < +1) < 10 ? '0' : '') + (value.date_from.getMonth() + 1);
+        var dd = value.date_from.getDate();
+        var yy = value.date_from.getFullYear();
+        value.date_from = yy + '.' + mm + '.' + dd;
+
+
+        var mm = ((value.date_to.getMonth() < +1) < 10 ? '0' : '') + (value.date_to.getMonth() + 1);
+        var dd = value.date_to.getDate();
+        var yy = value.date_to.getFullYear();
+        value.date_to = yy + '.' + mm + '.' + dd;
+
+    }
+
+
+    if (value.court) {
+        value.court = value.court.id
+    }
+
+    if (value.category) {
+        value.category = value.category.id;
+    }
+    if (value.result) {
+        value.result = value.result.id;
+    }
+
     console.log(value);
 
 
     $scope.getResponse = function () {
+
+
         $http({
             url: 'https://api.zandylyq.kz/v1/civil/request',
             method: 'POST',
-            // data: sendBodyObj,
+            data: value,
             cache: false,
             contentType: false,
             async: true,
@@ -93,7 +182,6 @@ var modalCivil = function ($scope, $uibModalInstance, $http, value) {
                 "X-Requested-With": "XMLHttpRequest"
             }
         }).then(function (value) {
-            // console.log(value);
             $scope.tableData = value.data;
             console.log($scope.tableData)
         }, function (reason) {
@@ -102,7 +190,6 @@ var modalCivil = function ($scope, $uibModalInstance, $http, value) {
 
         $uibModalInstance.close();
     };
-
     $scope.getResponse();
 
 
