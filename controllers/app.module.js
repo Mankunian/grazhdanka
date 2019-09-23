4baa5dcb7d528221096ffc5ae6e46b002f98bf8a
web-dev <dias.07.04@gmail.com>
2019-09-23 17:31:59 +0600

    grazhdanka

--- controllers/app.module.js
+++ controllers/app.module.js
@@ -11,15 +11,16 @@
 
 angular.module('app').config(function ($routeProvider) {
     $routeProvider
+        .when("/controllers/new_form", {
+            templateUrl: "new_form.html"
+        })
         .when("/controllers/index", {
             templateUrl: "index.html"
         })
         .when("/controllers/civil", {
             templateUrl: "civil.html"
         })
-        .when("/controllers/new_form", {
-            templateUrl: "new_form.html"
-        })
+
 
         .otherwise({redirectTo: '/'});
 });
